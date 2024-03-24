c402c68757b30d6ff4ebd30c2d6c2413818a2688
georgijbudik <georgijbudik@gmail.com>
2024-03-25 00:58:16 +0100

    feat: add reusable title component

---
+++ src/components/ui/title.tsx
@@ -0,0 +1,9 @@
+const Title = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <h1 className="bg-gradient-to-b from-neutral-200  to-neutral-600 bg-clip-text text-center font-sans text-lg  font-bold text-transparent md:text-7xl">
+      {children}
+    </h1>
+  );
+};
+
+export default Title;
