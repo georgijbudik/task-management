4dd6610035250a37831b3ecb4ef8e86ea29902af
georgijbudik <georgijbudik@gmail.com>
2024-03-25 00:57:05 +0100

    feat: add auth link component

---
+++ src/app/_components/auth-link.tsx
@@ -0,0 +1,16 @@
+import { getServerAuthSession } from "@/server/auth";
+import Link from "next/link";
+
+const AuthLink = async () => {
+  const session = await getServerAuthSession();
+  return (
+    <Link
+      href={session ? "/api/auth/signout" : "/api/auth/signin"}
+      className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+    >
+      {session ? "Sign out" : "Sign in"}
+    </Link>
+  );
+};
+
+export default AuthLink;
