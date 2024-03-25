57766befe88894e0d9871eadc0633f23685153a0
georgijbudik <georgijbudik@gmail.com>
2024-03-25 01:51:59 +0100

    feat: add alert if no tasks

--- src/app/_components/tasks-list.tsx
+++ src/app/_components/tasks-list.tsx
@@ -5,18 +5,27 @@ import TasksListItem from "./tasks-list-item";
 const TasksList = async () => {
   const tasks = await api.task.getAll();
   return (
-    <ul className="flex w-[400px] flex-col gap-4">
-      {tasks.map(({ id, title, description, completed }) => {
-        return (
-          <TasksListItem
-            id={id}
-            title={title}
-            description={description!}
-            completed={completed}
-          />
-        );
-      })}
-    </ul>
+    <>
+      <ul className="flex w-[400px] flex-col gap-4">
+        {tasks.map(({ id, title, description, completed }) => {
+          return (
+            <TasksListItem
+              id={id}
+              title={title}
+              description={description!}
+              completed={completed}
+            />
+          );
+        })}
+      </ul>
+      {tasks.length === 0 && (
+        <div className="w-[350px]">
+          <h2 className="bg-gradient-to-tr from-neutral-200 to-neutral-400 bg-clip-text text-center font-sans font-bold text-transparent md:text-3xl">
+            You do not have any created tasks yet
+          </h2>
+        </div>
+      )}
+    </>
   );
 };
 
