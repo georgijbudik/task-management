b8354c9f9417c1e7dd9b96b16db3ba5c4f0e2969
georgijbudik <georgijbudik@gmail.com>
2024-03-25 03:17:33 +0100

    fix: make title color lighter

--- src/components/ui/title.tsx
+++ src/components/ui/title.tsx
@@ -1,6 +1,6 @@
 const Title = ({ children }: { children: React.ReactNode }) => {
   return (
-    <h1 className="bg-gradient-to-b from-neutral-200  to-neutral-600 bg-clip-text text-center font-sans text-lg  font-bold text-transparent md:text-7xl">
+    <h1 className="bg-gradient-to-b from-neutral-200  to-neutral-400 bg-clip-text text-center font-sans text-lg  font-bold text-transparent md:text-7xl">
       {children}
     </h1>
   );
